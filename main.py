f244b505acc9c394eb3c2897bb211883ecedd1f4
Christopher <cguevaralopez@mail.weber.edu>
2024-11-19 18:15:24 -0700

    create vehicle

---
+++ main.py
@@ -0,0 +1,7 @@
+from vehicle import Vehicle
+
+if __name__ == "__main__":
+    #create an instance of the Vehicle class
+    black_car = Vehicle("Tesla", "Model 3", 2018)
+
+    print(black_car)
--- vehicle.py
+++ vehicle.py
@@ -8,3 +8,9 @@ class Vehicle:
         self.make = make
         self.model = model
         self.year = year
+
+
+    # instance method that we can perform on the object
+    def __str__(self):
+        #return a string representing the vehicle object
+        return f"Vehicle: {self.make}, Model {self.model}, Year: {self.year}"
